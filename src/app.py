a3e246c062f9d295e4903cc9e76ab6bccdb18d0e
Berenice Ramos <50807609+Bere333@users.noreply.github.com>
2025-06-27 19:21:03 +0000

    added user and database

--- src/app.py
+++ src/app.py
@@ -1,13 +1,24 @@
-import os
-import pandas as pd
-from sqlalchemy import create_engine, text
-from dotenv import load_dotenv
+# import os
+# import pandas as pd
+# from sqlalchemy import create_engine, text
+# from dotenv import load_dotenv
+# import pandas as pd
+
+# load_dotenv
+
+# connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+# print("Starting the connection...")
+# engine = create_engine(connection_string, isolation_level="AUTOCOMMIT")
+# engine.connect()
+# print("Connected successfully!")
 
 # Load environment variables
 
 # 1) Connect to the database with SQLAlchemy
+#engine = engine = create_engine('postgresql+psycopg2://username:password@localhost/dbname')
 
 # 2) Create the tables
+
 
 # 3) Insert data
 
